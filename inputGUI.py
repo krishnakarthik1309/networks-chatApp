01e6f184f4fd32ff22404e259b03c01597078d71
shzzb <hydershahzeb007@gmail.com>
2017-11-10 22:33:21 +0530

    better looking input box

--- inputGUI.py
+++ inputGUI.py
@@ -7,21 +7,25 @@ from client import messagePacket
 class mainWindow(object):
     def __init__(self,master, server, userDetails):
         self.master=master
+        master.minsize(width=640, height=480)
         self.server=server
         self.userDetails=userDetails
         self.l=Label(master,text="Enter your command here")
         self.l.pack()
-        self.e=Entry(master)
+        self.e = Text(master, height=15, width=100)
+        self.e.configure(font=("Helvetica", 12))
         self.e.pack()
         self.b=Button(master,text='Ok',command=self.sendToMain)
         self.b.pack()
+        self.master.title("CS425A Chat Application :\t Client \t{}(you)".format(userDetails['username']))
+
 
     def sendToMain(self, event=None):
-        message = self.e.get()
+        message = self.e.get(1.0,END)
         if message == 'logout':
             self.master.quit()
         sys.stdout.write("<{}(you)>:  {}\n".format(self.userDetails['username'], message))
-        self.e.delete(0, 'end')
+        self.e.delete(1.0, 'end')
         packet = messagePacket(message.strip().split(), self.userDetails)
         if not packet:
             return
